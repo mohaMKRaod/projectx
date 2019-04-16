fd72b23afc1d3a3e5aeb28180aaac5116ee4c24c
mohaMKRaod <mohamedkhaledaodmkr@gmail.com>
2019-04-16 19:48:45 +0200

    Update index.blade.php

--- resources/views/Products/index.blade.php
+++ resources/views/Products/index.blade.php
@@ -10,7 +10,7 @@
 
     <table class="table table-bordered">
         <tr>
-            <th with="80px">No</th>
+            <th with="80px">Nof</th>
             <th>Title</th>
             <th>Price</th>
             <th>Quantity</th>
