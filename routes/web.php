09df596b03ac761d5ceccd08e86e61411eb66b6d
mohaMKRaod <mohamedkhaledaodmkr@gmail.com>
2019-05-01 22:12:59 +0200

    middel

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('Products','ProductsController');
+Route::resource('Products','ProductsController')->middleware('mangeracses') ;
